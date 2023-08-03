04c73e9595bd429a7ba80bf6f2b06af759e130dd
Ahmet <ahmetcaakirr@gmail.com>
2023-08-03 23:35:55 +0300

    added MediatR library

---
+++ src/rentACar/RentACar/Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+
+namespace Application.Features.Brands.Commands.Create;
+
+public class CreateBrandCommand : IRequest<CreatedBrandResponse>
+{
+    public string Name { get; set; }
+
+    public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, CreatedBrandResponse>
+    {
+        public Task<CreatedBrandResponse>? Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+        {
+            CreatedBrandResponse response = new ();
+            response.Name = request.Name;
+            response.Id = new Guid();
+            return null;
+        }
+    }
+}
