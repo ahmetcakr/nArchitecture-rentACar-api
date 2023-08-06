ce27edd66d1379dd96e7e8ced67bdbef4967c5ab
Ahmet <ahmetcaakirr@gmail.com>
2023-08-06 23:49:34 +0300

    implemented EfRepositoryBase

--- src/corePackages/Core.Packages/Core.Persistence/Paging/Paginate.cs
+++ src/corePackages/Core.Packages/Core.Persistence/Paging/Paginate.cs
@@ -10,7 +10,7 @@ public class Paginate<T>
 {
     public Paginate()
     {
-        Items = Array.Empty<T>(); 
+        Items = Array.Empty<T>();
     }
 
     public int Size { get; set; }
@@ -18,7 +18,7 @@ public class Paginate<T>
     public int Count { get; set; }
     public int Pages { get; set; }
     public IList<T> Items { get; set; }
-    public bool HasPrevious => Index > 0;
-    public bool HasNext => Index + 1 < - Pages;  
 
+    public bool HasPrevious => Index > 0;
+    public bool HasNext => Index + 1 < Pages;
 }
