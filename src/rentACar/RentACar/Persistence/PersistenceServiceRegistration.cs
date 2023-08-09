313f8f5e68e5941c85581716a1011ea6b42165a5
Ahmet <ahmetcaakirr@gmail.com>
2023-08-10 00:51:36 +0300

    add another databases and initialize sql server

--- src/rentACar/RentACar/Persistence/PersistenceServiceRegistration.cs
+++ src/rentACar/RentACar/Persistence/PersistenceServiceRegistration.cs
@@ -11,7 +11,11 @@ public static class PersistenceServiceRegistration
 {
     public static IServiceCollection AddPersistenceServices(this IServiceCollection services,IConfiguration configuration)
     {
-        services.AddDbContext<BaseDbContext>(options => options.UseInMemoryDatabase("nArchitecture"));
+        //services.AddDbContext<BaseDbContext>(options => options.UseInMemoryDatabase("nArchitecture"));
+
+        services.AddDbContext<BaseDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnectionString")));
+        
+        
         services.AddScoped<IBrandRepository, BrandRepository>();
         return services;
     }
