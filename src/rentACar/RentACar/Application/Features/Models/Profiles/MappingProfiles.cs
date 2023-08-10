4f127ebcaee9e24000b854b95e36ca7737cd76c9
Ahmet <ahmetcaakirr@gmail.com>
2023-08-11 00:31:04 +0300

    implement Business Rules

--- src/rentACar/RentACar/Application/Features/Models/Profiles/MappingProfiles.cs
+++ src/rentACar/RentACar/Application/Features/Models/Profiles/MappingProfiles.cs
@@ -1,4 +1,5 @@
 ﻿using Application.Features.Models.Queries.GetList;
+using Application.Features.Models.Queries.GetListByDynamic;
 using AutoMapper;
 using Core.Application.Responses;
 using Core.Persistence.Paging;
@@ -11,7 +12,15 @@ public class MappingProfiles:Profile
     public MappingProfiles()
     {
         CreateMap<Model, GetListModelListItemDto>().ReverseMap();
+        CreateMap<Model, GetListByDynamicModelListItemDto>()
+            .ForMember(destinationMember: c => c.BrandName, memberOptions: opt => opt.MapFrom(src => src.Brand.Name))
+            .ForMember(destinationMember: c => c.FuelName, memberOptions: opt => opt.MapFrom(src => src.Fuel.Name))
+            .ForMember(destinationMember: c => c.TransmissionName, memberOptions: opt => opt.MapFrom(src => src.Transmission.Name))
+            .ReverseMap();
+
         CreateMap<Paginate<Model>,GetListResponse<GetListModelListItemDto> >().ReverseMap();
+
+        CreateMap<Paginate<Model>,GetListResponse<GetListByDynamicModelListItemDto> >().ReverseMap();
 
         // Eğer Models içerisindeki bir property ile başka bir entity'nin property'sini eşleştirmek istiyorsak
         // örneğin Model içerisindeki BrandId property'sini BrandName property'si ile eşleştirmek istiyorsak
--- src/rentACar/RentACar/Application/Features/Models/Queries/GetListByDynamic/GetListByDynamicModelListItemDto.cs
+++ src/rentACar/RentACar/Application/Features/Models/Queries/GetListByDynamic/GetListByDynamicModelListItemDto.cs
@@ -4,15 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Application.Features.Models.Queries.GetList;
+namespace Application.Features.Models.Queries.GetListByDynamic;
 
-public class GetListModelListItemDto
+public class GetListByDynamicModelListItemDto
 {
     public Guid Id { get; set; }
     public string FuelName { get; set; }
-    public string TransmissionName{ get; set; }
+    public string TransmissionName { get; set; }
     public string BrandName { get; set; }
     public string Name { get; set; }
     public decimal DailyPrice { get; set; }
-    public string ImageUrl{ get; set; }
+    public string ImageUrl { get; set; }
 }
